a77b56bb7d4ba71a8fa9966dec65c10dc49ccdd2
christian <christian@192.168.1.104>
2017-11-25 02:46:24 -0500

    commit1

---
+++ src/main/resources/ec/edu/ups/appdis/fastfood/modelo/Usuario.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.appdis.fastfood.modelo;
+
+public class Usuario {
+private String codigo;
+private String cedula;
+private String nombre;
+private String apellido;
+private String fecheRegist;
+private String direccion;
+private String telefono;
+
+}
